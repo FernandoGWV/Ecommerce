2cfe84f270aba3f0c14b1a3bf9fad3c50e5ae657
Luiz Fernando <luizwinther09@gmail.com>
2022-08-05 08:15:15 -0300

    Teste na fase de produtos

--- src/Components/Produto.js
+++ src/Components/Produto.js
@@ -2,19 +2,18 @@ import React from "react";
 
 import InfoProduto from "./InfoProduto.js";
 import styles from "./StylesComponents/Produto.module.css";
-import Product1 from "../Assets/image-product-1.jpg";
-import Product2 from "../Assets/image-product-2.jpg";
-import Product3 from "../Assets/image-product-3.jpg";
-import Product4 from "../Assets/image-product-4.jpg";
-import Product1Thumb from "../Assets/image-product-1-thumbnail.jpg";
-import Product2Thumb from "../Assets/image-product-2-thumbnail.jpg";
-import Product3Thumb from "../Assets/image-product-3-thumbnail.jpg";
-import Product4Thumb from "../Assets/image-product-4-thumbnail.jpg";
 import ModalProduto from "./ModalProduto";
-const Produto = () => {
+const Produto = ({
+  titulo,
+  description,
+  preco,
+  promo,
+  desconto,
+  OthersImage,
+}) => {
   const [modal, setModal] = React.useState(null);
-  const [product, setProduct] = React.useState(Product1);
-  const img = React.useRef(Product1);
+  const [product, setProduct] = React.useState(OthersImage[0]?.linkLarge);
+  const img = React.useRef(OthersImage[0]?.linkLarge);
   return (
     <>
       <div className={styles.ArtgProduto}>
@@ -28,35 +27,18 @@ const Produto = () => {
             />
           </div>
           <ul>
-            <li>
-              <img
-                src={Product1Thumb}
-                onClick={() => setProduct(Product1)}
-                id="Produto1"
-                alt="Produto thumbanil 1"
-              />
-            </li>
-            <li>
-              <img
-                src={Product2Thumb}
-                onClick={() => setProduct(Product2)}
-                alt="Produto thumbnail 2"
-              />
-            </li>
-            <li>
-              <img
-                src={Product3Thumb}
-                onClick={() => setProduct(Product3)}
-                alt="Produto thumbnail 3"
-              />
-            </li>
-            <li>
-              <img
-                src={Product4Thumb}
-                onClick={() => setProduct(Product4)}
-                alt="Produto thumbnail 4"
-              />
-            </li>
+            {OthersImage.map((item) => {
+              return (
+                <li>
+                  <img
+                    src={OthersImage.linkSmall}
+                    onClick={() => setProduct(OthersImage.linkLarge)}
+                    id="Produto1"
+                    alt="Produto thumbanil 1"
+                  />
+                </li>
+              );
+            })}
           </ul>
         </div>
         <InfoProduto img={img} />
---
+++ src/ProdutoDataBase/ProdutosDB.js
@@ -0,0 +1,40 @@
+import Product1 from "../Assets/image-product-1.jpg";
+import Product2 from "../Assets/image-product-2.jpg";
+import Product3 from "../Assets/image-product-3.jpg";
+import Product4 from "../Assets/image-product-4.jpg";
+import Product1Thumb from "../Assets/image-product-1-thumbnail.jpg";
+import Product2Thumb from "../Assets/image-product-2-thumbnail.jpg";
+import Product3Thumb from "../Assets/image-product-3-thumbnail.jpg";
+import Product4Thumb from "../Assets/image-product-4-thumbnail.jpg";
+
+const ProdutosDB = [
+  {
+    titulo: "Fall Limited Edition Sneakers",
+    id: 1,
+    description:
+      "These low-pr ofile sneakrs are you perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer",
+    preco: 250.0,
+    promo: 125.0,
+    desconto: 50,
+    OthersImage: [
+      {
+        linkSmall: Product1Thumb,
+        linkLarge: Product1,
+      },
+      {
+        linkSmall: Product2Thumb,
+        linkLarge: Product2,
+      },
+      {
+        linkSmall: Product3Thumb,
+        linkLarge: Product3,
+      },
+      {
+        linkSmall: Product4Thumb,
+        linkLarge: Product4,
+      },
+    ],
+  },
+];
+
+export default ProdutosDB;
